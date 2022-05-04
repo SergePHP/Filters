8e177488d8a047b77fe5c51a6b381cd1a1a70a20
Sergei Mishin <serge500@mail.ru>
2022-05-04 15:53:04 +0300

    return to work

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -21,9 +21,16 @@ using System.Windows.Media.Effects;
 
 namespace WpfApp3
 {
+    public struct RGB
+    {
+        public float R;
+        public float G;
+        public float B;
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
         internal BitmapSource orirginalSourceBitmap = null;
@@ -246,48 +253,93 @@ namespace WpfApp3
             UInt32[,] pixels = new UInt32[height, width];
             UInt32[,] transformedPixels = new UInt32[height, width];
 
-            int i = 0;
+            int index = 0;
             for (int h = 0; h < height; h++)
-                for (int w = 0; w < width; w++, i += bytesPerPixel)
-                    pixels[h, w] = BitConverter.ToUInt32(bytes, i);
+                for (int w = 0; w < width; w++, index += bytesPerPixel)
+                    pixels[h, w] = BitConverter.ToUInt32(bytes, index);
 
             // ---------------------------------------------------------------------------------
 
-            int i, j, k, m, gap = (int)(N / 2);
-            int tmpH = H + 2 * gap, tmpW = W + 2 * gap;
+            int i, j, k, m, gap = (int)(matrixSize / 2);
+            int tmpH = height + 2 * gap, tmpW = width + 2 * gap;
             UInt32[,] tmppixel = new UInt32[tmpH, tmpW];
-            UInt32[,] newpixel = new UInt32[H, W];
+            //UInt32[,] newpixel = new UInt32[height, width];
             //заполнение временного расширенного изображения
             //углы
             for (i = 0; i < gap; i++)
                 for (j = 0; j < gap; j++)
                 {
-                    tmppixel[i, j] = pixel[0, 0];
-                    tmppixel[i, tmpW - 1 - j] = pixel[0, W - 1];
-                    tmppixel[tmpH - 1 - i, j] = pixel[H - 1, 0];
-                    tmppixel[tmpH - 1 - i, tmpW - 1 - j] = pixel[H - 1, W - 1];
+                    tmppixel[i, j] = pixels[0, 0];
+                    tmppixel[i, tmpW - 1 - j] = pixels[0, width - 1];
+                    tmppixel[tmpH - 1 - i, j] = pixels[height - 1, 0];
+                    tmppixel[tmpH - 1 - i, tmpW - 1 - j] = pixels[height - 1, width - 1];
                 }
             //крайние левая и правая стороны
             for (i = gap; i < tmpH - gap; i++)
                 for (j = 0; j < gap; j++)
                 {
-                    tmppixel[i, j] = pixel[i - gap, j];
-                    tmppixel[i, tmpW - 1 - j] = pixel[i - gap, W - 1 - j];
+                    tmppixel[i, j] = pixels[i - gap, j];
+                    tmppixel[i, tmpW - 1 - j] = pixels[i - gap, width - 1 - j];
                 }
             //крайние верхняя и нижняя стороны
             for (i = 0; i < gap; i++)
                 for (j = gap; j < tmpW - gap; j++)
                 {
-                    tmppixel[i, j] = pixel[i, j - gap];
-                    tmppixel[tmpH - 1 - i, j] = pixel[H - 1 - i, j - gap];
+                    tmppixel[i, j] = pixels[i, j - gap];
+                    tmppixel[tmpH - 1 - i, j] = pixels[height - 1 - i, j - gap];
                 }
             //центр
-            for (i = 0; i < H; i++)
-                for (j = 0; j < W; j++)
-                    tmppixel[i + gap, j + gap] = pixel[i, j];
+            for (i = 0; i < height; i++)
+                for (j = 0; j < width; j++)
+                    tmppixel[i + gap, j + gap] = pixels[i, j];
 
+            //применение ядра свертки
+            RGB ColorOfPixel = new RGB();
+            RGB ColorOfCell = new RGB();
+            for (i = gap; i < tmpH - gap; i++)
+                for (j = gap; j < tmpW - gap; j++)
+                {
+                    ColorOfPixel.R = 0;
+                    ColorOfPixel.G = 0;
+                    ColorOfPixel.B = 0;
+                    for (k = 0; k < matrixSize; k++)
+                        for (m = 0; m < matrixSize; m++)
+                        {
+                            ColorOfCell = CalculationOfColor(tmppixel[i - gap + k, j - gap + m], blur[k, m]);
+                            ColorOfPixel.R += ColorOfCell.R;
+                            ColorOfPixel.G += ColorOfCell.G;
+                            ColorOfPixel.B += ColorOfCell.B;
+                        }
+                    //контролируем переполнение переменных
+                    if (ColorOfPixel.R < 0) ColorOfPixel.R = 0;
+                    if (ColorOfPixel.R > 255) ColorOfPixel.R = 255;
+                    if (ColorOfPixel.G < 0) ColorOfPixel.G = 0;
+                    if (ColorOfPixel.G > 255) ColorOfPixel.G = 255;
+                    if (ColorOfPixel.B < 0) ColorOfPixel.B = 0;
+                    if (ColorOfPixel.B > 255) ColorOfPixel.B = 255;
 
+                    transformedPixels[i - gap, j - gap] = Build(ColorOfPixel);
+                }
+
+            //return transformedPixels;
         }
+        //сборка каналов
+        public static UInt32 Build(RGB ColorOfPixel)
+        {
+            UInt32 Color;
+            Color = 0xFF000000 | ((UInt32)ColorOfPixel.R << 16) | ((UInt32)ColorOfPixel.G << 8) | ((UInt32)ColorOfPixel.B);
+            return Color;
+        }
+        //вычисление нового цвета
+        public static RGB CalculationOfColor(UInt32 pixel, double coefficient)
+        {
+            RGB Color = new RGB();
+            Color.R = (float)(coefficient * ((pixel & 0x00FF0000) >> 16));
+            Color.G = (float)(coefficient * ((pixel & 0x0000FF00) >> 8));
+            Color.B = (float)(coefficient * (pixel & 0x000000FF));
+            return Color;
+        }
+
         //public static Bitmap ConvertToBitmap(BitmapSource bitmapSource)
         //{
         //    var width = bitmapSource.PixelWidth;
